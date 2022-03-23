519a8f8efedaa9b73a650e06a26560b23c6db9bb
prateek <stupidcoder22@gmail.com>
2022-03-23 17:58:31 +0530

    Alert page done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import NoteState from "./Context/Notes/NoteState";
+import { Alert } from "./Components/Alert";
 
 function App() {
   return (
     <>
       <NoteState>
         <Navbar />
+        <Alert message="This is amazing React Website" />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
--- src/Components/Noteitem.jsx
+++ src/Components/Noteitem.jsx
@@ -4,12 +4,11 @@ const Noteitem = ({ note }) => {
   return (
     <div className="col-md-3">
       <div className="card my-3">
-        <img src="..." className="card-img-top" alt="..." />
         <div className="card-body">
           <div className="d-flex align-items-center">
             <h5 className="card-title">{note.title}</h5>
-            <i className="fa-solid fa-trash mx-2"></i>
-            <i className="fa-solid fa-file-pen mx-3"></i>
+            <i className="fa-solid fa-trash ms-5"></i>
+            <i className="fa-solid fa-file-pen ms-4"></i>
           </div>
           <p className="card-text">{note.description}</p>
         </div>
